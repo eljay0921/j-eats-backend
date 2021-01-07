781642488cc93cc1229809711485f19649c340c5
jin <lj7812@gmail.com>
2021-01-07 20:26:56 +0900

    #1.4 Understanding GraphQL - Args

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,11 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurants.entity';
 
 @Resolver(of => Restaurant)
 export class RestaurantsResolver {
-  @Query(() => Restaurant)
-  myRestaurant() {
-    return true;
+  @Query(() => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: Boolean): Restaurant[] {
+    console.log(veganOnly);
+    return [];
   }
 }
