de90beac91ac465ece4a3432a182110c2ae58044
jin <lj7812@gmail.com>
2021-01-10 15:39:38 +0900

    #2.3 Applying ENV

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,14 +10,15 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       isGlobal: true,
       envFilePath:
         process.env.NODE_ENV === 'dev' ? './envs/.env.dev' : './envs/.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'imjin',
-      password: 'wls0921', // 사실 localhost로 연결할 때는 비말번호를 묻지 않기 때문에 입력하지 않아도 됨
-      database: 'j-eats',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD, // 사실 localhost로 연결할 때는 비말번호를 묻지 않기 때문에 입력하지 않아도 됨
+      database: process.env.DB_NAME,
       synchronize: true, // DB에 연결할 때, DB를 현재 모듈의 상태로 마이그레이션
       logging: true,
     }),
