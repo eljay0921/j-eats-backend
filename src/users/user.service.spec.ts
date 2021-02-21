cfb776ea8466da4d02324218f9f0e70714d56c05
jin <lj7812@gmail.com>
2021-02-21 16:39:19 +0900

    #7.3 Create Account Testing (2)

--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -28,6 +28,7 @@ describe('UserService', () => {
   let service: UsersService;
   let usersRepository: MockRepository<User>; // UserRepository의 모든 함수를 가져오기 위함
   let verificationRepository: MockRepository<Verification>;
+  let mailService: MailService;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
@@ -53,6 +54,7 @@ describe('UserService', () => {
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    mailService = module.get<MailService>(MailService);
     usersRepository = module.get(getRepositoryToken(User));
     verificationRepository = module.get(getRepositoryToken(Verification));
   });
@@ -68,7 +70,7 @@ describe('UserService', () => {
       role: 0,
     };
 
-    // 중복 체크되어 생성에 실패하는 테스트
+    // 이메일이 중복 체크되어 계정 생성에 실패하는 테스트
     it('should fail if user exists', async () => {
       usersRepository.findOne.mockResolvedValue({
         id: 1,
@@ -82,15 +84,23 @@ describe('UserService', () => {
       });
     });
 
-    // 생성에 성공하는 테스트
+    // 계정 생성에 성공하는 테스트
     it('should create a new user', async () => {
+      // 각 기능이 어떤 결과를 반환할지 mock으로 만든다.
       usersRepository.findOne.mockReturnValue(undefined);
       usersRepository.create.mockReturnValueOnce(createAccountArgs);
       usersRepository.save.mockResolvedValue(createAccountArgs);
-      verificationRepository.create.mockReturnValue(createAccountArgs);
+      verificationRepository.create.mockReturnValue({
+        user: createAccountArgs,
+      });
+      verificationRepository.save.mockResolvedValue({
+        code: 'code',
+      });
 
-      await service.createAccount(createAccountArgs);
+      // 테스트할 함수를 호출하고
+      const result = await service.createAccount(createAccountArgs);
 
+      // 각 mock repository의 결과를 예상(테스트)한다.
       expect(usersRepository.create).toHaveBeenCalledTimes(1); // 이 함수가 1번만 호출될거라고 예상하는 테스트
       expect(usersRepository.create).toHaveBeenCalledWith(createAccountArgs);
       expect(usersRepository.save).toHaveBeenCalledTimes(1);
@@ -101,11 +111,20 @@ describe('UserService', () => {
         user: createAccountArgs,
       });
       expect(verificationRepository.save).toHaveBeenCalledTimes(1);
-      expect(verificationRepository.save).toHaveBeenCalledWith(
-        createAccountArgs,
+      expect(verificationRepository.save).toHaveBeenCalledWith({
+        user: createAccountArgs,
+      });
+
+      expect(mailService.sendVerificationEmail).toHaveBeenCalled();
+      expect(mailService.sendVerificationEmail).toHaveBeenCalledWith(
+        expect.any(String),
+        expect.any(String),
       );
+
+      expect(result).toEqual({ ok: true });
     });
   });
+
   it.todo('login');
   it.todo('findById');
   it.todo('editProfile');
