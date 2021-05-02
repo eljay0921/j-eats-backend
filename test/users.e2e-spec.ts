570235bdd238414d9366807de1b0f52a1dff3876
jin <lj7812@gmail.com>
2021-05-02 16:57:18 +0900

    Initialize E2E Testing (2)

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { getConnection } from 'typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -16,13 +17,14 @@ describe('AppController (e2e)', () => {
   });
 
   afterAll(async () => {
+    await getConnection().dropDatabase();
     app.close();
   });
 
-  it.todo('me');
-  it.todo('userProfile');
   it.todo('createAccount');
+  it.todo('userProfile');
   it.todo('login');
-  it.todo('editProfile');
+  it.todo('me');
   it.todo('verifyEmail');
+  it.todo('editProfile');
 });
